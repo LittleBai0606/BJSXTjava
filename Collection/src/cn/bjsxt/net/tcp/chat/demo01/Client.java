feec9b936973726c134fd638b419d3c42960dce5
Ben <958255724@qq.com>
2017-07-01 17:19:16 +0800

    初步实现简单的聊天室

--- Collection/src/cn/bjsxt/net/tcp/chat/demo01/Client.java
+++ Collection/src/cn/bjsxt/net/tcp/chat/demo01/Client.java
@@ -1,11 +1,29 @@
 package cn.bjsxt.net.tcp.chat.demo01;
 
+import javax.xml.crypto.Data;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 /**
+ * 1、创建客户端:发送数据+接收数据
+ * 2、写出数据 输出流
+ * 3、读取数据 输入流
  * Created by BenWhite on 2017/7/1.
  */
 public class Client {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        Socket client = new Socket("localhost", 9999);
+        //输出流
+        DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+        dos.writeUTF("数据");
+        dos.flush();
+
+        DataInputStream dis = new DataInputStream(client.getInputStream());
+        String msg = dis.readUTF();
+        System.out.println("收到的数据为:" + msg);
     }
 }
--- Collection/src/cn/bjsxt/net/tcp/chat/demo01/Server.java
+++ Collection/src/cn/bjsxt/net/tcp/chat/demo01/Server.java
@@ -1,11 +1,33 @@
 package cn.bjsxt.net.tcp.chat.demo01;
 
+import sun.misc.Cleaner;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
+ * 1、创建服务器
+ * 2、写出数据 输出流
+ * 3、读取数据 输入流
  * Created by BenWhite on 2017/7/1.
  */
 public class Server {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket server = new ServerSocket(9999);
+        Socket client = server.accept();
+
+        DataInputStream dis = new DataInputStream(client.getInputStream());
+        String msg = dis.readUTF();
+        System.out.println("收到的数据为:" + msg);
+
+        DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+        dos.writeUTF("服务器-->" + msg);
+        dos.flush();
 
     }
 }
